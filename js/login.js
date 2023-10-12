41dd6e750a6ac42a8f5dec1606a11c7c7f48f60d
Fresinsya <vicafresinsya@gmail.com>
2023-10-12 10:25:46 +0700

    done2

--- js/login.js
+++ js/login.js
@@ -45,9 +45,39 @@ button.addEventListener('click', function (event) {
         return user.username === emailValue && user.password === passwordValue
       })
       if (user) {
-        alert('Login success')
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+        
+        Toast.fire({
+          icon: 'success',
+          title: 'Login successfully'
+        })
       } else {
-        alert('Login failed')
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+        
+        Toast.fire({
+          icon: 'warning',
+          title: 'silahkan cek kembali email dan passwordnya'
+        })
       }
     })
 
