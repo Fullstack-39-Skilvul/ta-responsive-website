d83d9e9007d2a9a8d0914d6ec9d520cfe6fdf2cb
Fresinsya <vicafresinsya@gmail.com>
2023-10-17 11:27:58 +0700

    done

--- js/payment.js
+++ js/payment.js
@@ -7,3 +7,18 @@ console.log(user)
 if (!user) {
     window.location.href = 'login.html';
 }
+
+function sukses() {
+    Swal.fire('Pembayaran Berhasil', 'Terima kasih telah melakukan pembayaran', 'success');
+    Swal.fire({
+        title: 'Pembayaran Berhasil',
+        text: 'Terima kasih telah melakukan pembayaran',
+        icon: 'success',
+        confirmButtonText: 'Ok'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            window.location.href = '../index.html'
+        }
+    })
+    // window.location.href = './index.html'
+}
